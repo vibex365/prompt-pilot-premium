de76b2400fdbf8144f2bd0639faff86bf242a35f
vibex365 <vibex365@gmail.com>
2025-04-26 21:53:33 -0400

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,9 @@
+function toggleTheme() {
+  if (document.body.style.background === "white") {
+    document.body.style.background = "#121212";
+    document.body.style.color = "white";
+  } else {
+    document.body.style.background = "white";
+    document.body.style.color = "black";
+  }
+}
