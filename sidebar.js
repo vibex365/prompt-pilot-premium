589f2748069f1478456cb3f84b5ba470002045f7
vibex365 <vibex365@gmail.com>
2025-04-26 21:52:11 -0400

    Create sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,5 @@
+function showSection(sectionId) {
+  const sections = document.querySelectorAll('.section');
+  sections.forEach(sec => sec.classList.remove('active'));
+  document.getElementById(sectionId).classList.add('active');
+}
